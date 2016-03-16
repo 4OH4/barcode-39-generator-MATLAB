ad4570d4d1e270336310fa71cbe9e16e7e48ed49
4OH4 <rupert@rupertthomas.com>
2016-03-16 20:14:28 +0000

    Restructured, added options

--- demo.m
+++ demo.m
@@ -1,13 +1,14 @@
 % Demo: Generate and show a barcode in Code 39 format
 
-testString = 'fooBar';
+testString = '123456';
 
-barWidth = 5;   % pixels
-barHeight = 50; % pixels
+barWidth = 3;   % pixels
+barHeight = 100; % pixels
+appendTerminationMarkers = false;
 
 % Generate a logical array containing the barcode
-barcodeArray = generateBarcodeCode39(testString, barWidth, barHeight);
+barcodeArray = generateBarcodeCode39(testString, barWidth, barHeight, appendTerminationMarkers);
 
 % Generate an image of the barcode
 figure;
-imshow(barcodeArray);
+imshow(~barcodeArray);  % Have to NOT the array, other wise MATLAB displays 1 as black and 0 as white
