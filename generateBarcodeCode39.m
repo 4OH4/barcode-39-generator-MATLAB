f53454205aeefadb848cf9e7e8480ccf8fb0b531
4OH4 <rupert@rupertthomas.com>
2016-03-16 20:14:00 +0000

    Restructured, added options

--- generateBarcodeCode39.m
+++ generateBarcodeCode39.m
@@ -1,4 +1,4 @@
-function barcodeArray = generateBarcodeCode39(input, barWidth, barHeight)
+function barcodeArray = generateBarcodeCode39(input, barWidth, barHeight, appendTerminationMarkers)
 % Convert the input string into a logical array, containing a
 % barcode in Code 39 format
 %
@@ -14,13 +14,27 @@ end
 if ~exist('barHeight','var')
     barHeight = 10;
 end
+if ~exist('appendTerminationMarkers','var')
+    appendTerminationMarkers = true;
+end
 
-% Generate logical array for input sequence
-encoded = encodeStringCode39(input);
+% Send each character from the input to be encoded
+bitString = '';
+for i = 1:length(input)
+    bitString = [bitString,encodeCharacter(input(i))];
+end
+
+% Stop/stop characters
+if appendTerminationMarkers
+    bitString = [encodeCharacter('*'),bitString,encodeCharacter('*')]; 
+end
+
+% Convert string to logical vector
+logicalVector = logical(bitString(:)'-'0');    % Thanks Mischa Kim
 
 % 'Stretch' out to required bar width
-encoded_t = encoded';
-fullBarWidth = repmat(encoded_t(:),1,barWidth)';
+logicalVector_t = logicalVector';
+fullBarWidth = repmat(logicalVector_t(:),1,barWidth)';
 
 % 'Stretch' out to required bar height
 barcodeArray = repmat(fullBarWidth(:),1,barHeight)';
